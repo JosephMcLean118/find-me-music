2a876448a5e77412ee6138c4d03fb8955e6b2aab
JosephMclean118 <josephjmclean392@gmail.com>
2026-04-02 00:14:06 +0100

    Basic setup using lastMF Scrobbling API

--- find-me-music.sh
+++ find-me-music.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 # Pick a random genre, then get top 50 albums of that genre, then pick one of the 50.
 #
-# primaryGenreName is what we want to queryBy.
-# We want to find 50 top albums of some genre
-output=$(curl -s https://itunes.apple.com/search?term=Rock&media=music&entity=album&limit=200)
 
-albumName=$(echo "$output" | jq -r '.results[] | select(.collectionName != null) | .collectionName')
-echo "$output" | jq 'keys'       # Shows top-level keys
-echo "$output" | jq '.resultCount'  # How many results?
-echo "$albumName"
+# Load in API Keys
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+
+# Test Query to make sure API Works
+genre="Rock"
+output=$(curl -s "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=$genre&api_key=$LASTFM_API_KEY&format=json&limit=50")
+
 exit 1
