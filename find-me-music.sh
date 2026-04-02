d0e446d24f3d28d55428abcf7bfc576338bb4292
Joseph McLean <josephjmclean392@gmail.com>
2026-04-02 14:17:27 +0100

    fixed comments in .sh

--- find-me-music.sh
+++ find-me-music.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
-# Pick a random genre, then get top 50 albums of that genre, then pick one of the 50.
-#
+# Get the album name and name of artist for a random album
 
 # Load in API Keys
 if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
+# Set genres and pick a random genre
 genres=("Rock" "Disco" "Jazz" "Pop" "Folk" "Electronic" "Punk" "Hip-Hop")
 random_genre=$(shuf -e "${genres[@]}" -n 1)
 
-# Get first album returned from API
+# Get random album of 50 returned from API
 output=$(curl -s "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=$random_genre&api_key=$LASTFM_API_KEY&format=json&limit=50" \
   | jq -r --argjson idx $((RANDOM % 50)) '.albums.album[$idx]')
 
-# Want to add feature so users can pass in genre if they want, otherwise it randomly picks one, also ened to add helpstring
+# Parse album name and artist name from album
 album_name=$(echo "$output" | jq -r '.name')
 artist_name=$(echo "$output" | jq -r '.artist.name')
 
